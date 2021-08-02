6eb414a7ea8c5c00f060aa4b1e4091bdcbd09c67
sonam tripathi <st040192@gmail.com>
2021-08-02 17:52:18 +0530

    completed feedback pagination for user and admin

--- server/models/feedback.js
+++ server/models/feedback.js
@@ -5,12 +5,29 @@ const UserSchema = mongoose.Schema({
     type: String,
     required: true
   },
+  ipAddress:{
+    type:String,
+    required:true
+  },
+  os:{
+    type:String,
+    required:true
+  },
+  network:{
+    type:String,
+    required:true
+  },
+  browser:{
+    type:String,
+    required:true
+  },
   rating:{
     type: Number,
     required:true
   },
   message: {
     type: String,
+    maxlength:125,
     required: true
   }
   },
--- server/routes/feedback_routes.js
+++ server/routes/feedback_routes.js
@@ -6,9 +6,20 @@ const Feedbacks=require("../models/feedback")
 router.post("/feedback",
     async(req,res)=>{
       try{
-      const{fullName,rating,message}=req.body
+      const{fullName,
+        ipAddress,
+        os,
+        network,
+        browser,
+        rating,
+        message}=req.body
+
       feedback = new Feedbacks({
         fullName,
+        ipAddress,
+        os,
+        network,
+        browser,
         rating,
         message
     });
@@ -23,4 +34,45 @@ router.post("/feedback",
 
     })
 
+    // For User
+    // http://localhost:3030/api/feedback/:id?pageNo=1&size=10
+
+    router.get("/feedback/:id",
+    async(req,res)=>{
+      try{
+        var pageNo = parseInt(req.query.pageNo)
+        var size = parseInt(req.query.size);
+       
+        if(pageNo < 0 || pageNo === 0) {
+              response = {"error" : true,"message" : "invalid page number, should start with 1"};
+              return res.json(response)
+        }
+        let skip =size*(pageNo-1)
+        let limit = size;
+        let feedbacks = await Feedbacks.findById(req.params.id).skip(skip).limit(limit)
+        res.json(feedbacks)
+      }catch (err){
+        console.log(err.message);
+        res.status(500).send("Error in Fetching");
+      }
+  })
+
+  // For Admin
+  // http://localhost:3030/api/feedback/admin?pageNo=1&size=10
+
+  router.get("/feedback/admin",
+    async(req,res)=>{
+      var pageNo = parseInt(req.query.pageNo)
+      var size = parseInt(req.query.size);
+      try{
+        let skip =size*(pageNo-1)
+        let limit = size;
+        let feedbacks = await Feedbacks.find({}).skip(skip).limit(limit)
+        res.json({feedbacks})
+      }catch (err){
+        console.log(err.message);
+        res.status(500).send("Error in Fetching");
+      }
+  })
+  
 module.exports = router;
